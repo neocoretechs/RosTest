3c54e42948f1fc48d3a83e030abbe20cc198c792
neocoretechs <groffj@neocoretechs.com>
2021-04-04 14:15:21 -0700

    Updated docs

--- src/main/java/org/ros/ParameterServerTestNode.java
+++ src/main/java/org/ros/ParameterServerTestNode.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2011 Google Inc.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
-
 package org.ros;
 
 import org.apache.commons.logging.Log;
@@ -34,10 +18,11 @@ import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 
 /**
- * This node is used in rostest end-to-end integration tests with other client
- * libraries.
+ * Constructs a series of topics that match primitive standard Ros message data types, creates those datatypes,
+ * as instance variables, attempts to retrieve values for those those datatypes from the parameter server,
+ * and finally publishes those instances to their respective topics, or their default values if no parameter is availble.
  * 
- * @author kwc@willowgarage.com (Ken Conley)
+ * @author Jonathan Groff (C) NeoCoreTechs 2021
  */
 public class ParameterServerTestNode extends AbstractNodeMain {
   private static final Log log = LogFactory.getLog(ParameterServerTestNode.class);
